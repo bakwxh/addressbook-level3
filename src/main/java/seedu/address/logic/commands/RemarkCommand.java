e1dd2b7240f9f20eb6aad1d74aa772ee47a2e777
bakwxh <omega1653@gmail.com>
2019-09-26 03:06:10 +0800

    Amend checkstyle

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -4,6 +4,8 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -11,17 +13,11 @@ import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Remark;
 
-import java.util.List;
 
 /**
  * Changes the remark of an existing person in the address book.
  */
 public class RemarkCommand extends Command {
-
-    public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
-
-    private final Index index;
-    private final Remark remark;
 
     public static final String COMMAND_WORD = "remark";
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the remark of the person identified "
@@ -31,9 +27,11 @@ public class RemarkCommand extends Command {
             + PREFIX_REMARK + "[REMARK]\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + "r/ Likes to swim.";
-
     public static final String MESSAGE_ADD_REMARK_SUCCESS = "Added remark to Person: %1$s";
     public static final String MESSAGE_DELETE_REMARK_SUCCESS = "Removed remark from Person: %1$s";
+
+    private final Index index;
+    private final Remark remark;
 
     /**
      * @param index of the person in the filtered person list to edit the remark
@@ -45,8 +43,6 @@ public class RemarkCommand extends Command {
         this.index = index;
         this.remark = remark;
     }
-
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET = "Remark command not implemented yet";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
